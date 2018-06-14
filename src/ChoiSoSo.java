cc3fbd4f43ee63c8aadb1750bba9bd5674f5cbd4
1234567890 <phantiensy195@gmail.com>
2018-06-14 16:05:39 +0700

    add play xoso with cau lenh dieu kien

--- src/ChoiSoSo.java
+++ src/ChoiSoSo.java
@@ -3,43 +3,28 @@ import java.util.Scanner;
 
 public class ChoiSoSo {
     public static void main(String[] args) {
+        while (true) {
         Random rd = new Random();
         int so_dau = rd.nextInt(10);
         int so_thu_hai = rd.nextInt(10);
         int a, b;
         Scanner sc = new Scanner(System.in);
-        while (true) {
             System.out.print("Mời bạn nhập số thứ nhất: ");
             a = Integer.parseInt(sc.nextLine());
             System.out.print("Mời bạn nhập số thứ hai: ");
             b = Integer.parseInt(sc.nextLine());
 
-            if (a == so_dau) {
-                if (b == so_thu_hai) {
-                    System.out.println("Bạn đã trúng độc đắc. Số tiền bạn nhận được là: " + "10000$");
-                } else {
-                    System.out.println("Bạn đạ đoán trúng 1 số. Phần thưởng của bạn là: " + "1000$");
-                }
-            } else if (a != so_dau) {
-                if (b == so_thu_hai) {
-                    System.out.println("Bạn đạ đoán trúng 1 số. Phần thưởng của bạn là: " + "1000$");
-                } else {
-                    System.out.println("Bạn tạch rồi!");
-                }
-            } else if (a == so_thu_hai) {
-                if (b == so_dau) {
-                    System.out.println("Bạn đạ đoán trúng số ngược lại của giải thưởng. Phần thưởng của bạn là: " + "3000$");
-                } else {
-                    System.out.println("Bạn đạ đoán trúng 1 số. Phần thưởng của bạn là: " + "1000$");
-                }
-            } else if (a != so_thu_hai) {
-                if (b == so_dau) {
-                    System.out.println("Bạn đạ đoán trúng 1 số. Phần thưởng của bạn là: " + "1000$");
-                }
+            if (a == so_dau && b == so_thu_hai) {
+                System.out.println("Bạn đã trúng độc đắc. Số tiền bạn nhận được là: " + "10000$");
+            } else if (a == so_dau || b == so_dau || a == so_thu_hai || b == so_thu_hai) {
+                System.out.println("Bạn đạ đoán trúng 1 số. Phần thưởng của bạn là: " + "1000$");
+            }else {
+                System.out.println("Bạn tạch rồi!");
             }
 
             System.out.printf("%s%d%d", "Số bạn cần phải đoán là: ", so_dau, so_thu_hai);
             System.out.println();
+            System.out.println("-----------------------------------------------------");
         }
     }
 }
